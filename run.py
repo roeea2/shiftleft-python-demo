9a18620e2481790fc0493c311c36ce9f1876f817
roeea2 <roeea2@gmail.com>
2023-09-07 14:14:06 +0300

    Update run.py
    Adding something

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ app = create_app()
 @app.after_request
 def add_csp_headers(response):
     # vulnerability: Broken Access Control
+    AWS_ACCESS_KEY_ID="ASIAW375RVVHUEL2RWGF"
     response.headers['Access-Control-Allow-Origin'] = '*'
     # vulnerability: Security Misconfiguration
     response.headers['Content-Security-Policy'] = "script-src 'self' 'unsafe-inline'"
